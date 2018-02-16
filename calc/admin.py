90148e2d49b9e389feae63afa8fc986def2cc3c0
Sahil Singla <singla.sahil221@gmail.com>
2018-02-16 11:09:19 +0530

    Update admin.py

--- calc/admin.py
+++ calc/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import batch,subjects,branch
+from .models import batch,subjects,branch,hits
 # Register your models here.
 admin.site.register(batch)
 admin.site.register(subjects)
 admin.site.register(branch)
+admin.site.register(hits)
